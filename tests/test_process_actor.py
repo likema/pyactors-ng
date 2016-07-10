27a175fc8b0184211e53b00fb2eacdc5e24156e3
Like Ma <likemartinma@gmail.com>
2016-07-11 00:21:04 +0800

    Add process actor with timeout support.

---
+++ tests/test_process_actor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import time
+from os.path import abspath, dirname, join
+
+sys.path.insert(0, abspath(join(dirname(sys.argv[0]), '..')))
+from processactor import Actor  # noqa
+
+
+class Pinger(Actor):
+    def receive(self, message):
+        print(message)
+        time.sleep(2)
+        pong.send('ping')
+
+    def handle_timeout(self):
+        print('pinger timeout')
+
+
+class Ponger(Actor):
+    def receive(self, message):
+        print(message)
+        time.sleep(2)
+        ping.send('pong')
+
+    def handle_timeout(self):
+        print('ponger timeout')
+
+
+ping = Pinger(1)
+pong = Ponger(1)
+
+ping.start()
+pong.start()
+
+ping.send('start')
+
+pong.join()
+ping.join()
+# vim: set ts=4 sw=4 sts=4 et:
